30207322c2f6fefdb6a6a84c5ea517b5d6e1de96
Meghana-sk <meghana.meghanask.sk@gmail.com>
2022-03-27 18:37:56 +0530

    fix: Review comments

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,49 +4,48 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const Home = () => {
-    const [ productCategories, setProductCategories] = useState([]);
-    const getProductCategories = async() => {
-        try {
-            const productsData = await axios.get("/api/categories");
-            setProductCategories(productsData.data.categories);
-        } catch (error) {
-            console.error(error)
-        }
-    }
+  const [productCategories, setProductCategories] = useState([]);
 
-    useEffect(() => {
-        getProductCategories()
-    }, [])
-    return (
-        <div>
-            <main>
-                <section className="hero-section">
-                    <div className="hero-container">
-                        <div className="hero-content">
-                            <h1 className="highlight-text text-xl fw-900">CLEARANCE SALE</h1>
-                            <h3 className="fw-600">20th FEB - 2nd MAR</h3>
-                            <Link to="/products">
-                                <button className="btn btn-primary text-s">SHOP NOW</button>
-                            </Link>
-                            <h1 className="fw-600 text-l">40-70% OFF</h1>
-                        </div>
-                    </div>
-                </section>
+  useEffect(() => {
+    (async () => {
+      try {
+        const catgeroiesData = await axios.get("/api/categories");
+        if (catgeroiesData.status === 200)
+          setProductCategories(catgeroiesData.data.categories);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+  return (
+    <div>
+      <main>
+        <section className="hero-section">
+          <div className="hero-container">
+            <div className="hero-content">
+              <h1 className="highlight-text text-xl fw-900">CLEARANCE SALE</h1>
+              <h3 className="fw-600">20th FEB - 2nd MAR</h3>
+              <Link to="/products">
+                <button className="btn btn-primary text-s">SHOP NOW</button>
+              </Link>
+              <h1 className="fw-600 text-l">40-70% OFF</h1>
+            </div>
+          </div>
+        </section>
 
-                <section className="featured">
-                    <h2>Featured categories</h2>
-                    <div className="featured-container fw-900">
-                        { productCategories.map(({ categoryName }) => 
-                            (
-                                <Link to="/products">
-                                    <div className="category cat-one">{ categoryName }</div>
-                                </Link>
-                            )) }
-                    </div>
-                </section>
-            </main>
-        </div>
-    );
-}
+        <section className="featured">
+          <h2>Featured categories</h2>
+          <div className="featured-container fw-900">
+            {productCategories.map(({ categoryName }) => (
+              <Link to="/products">
+                <div className="category cat-one">{categoryName}</div>
+              </Link>
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
 
 export { Home };
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,33 +1,28 @@
-import "./products.css"
+import "./products.css";
 import { Card, FilterSideNav } from "../../components";
 import { useProducts } from "../../context/product-context";
 
 const Products = () => {
-    const { availableProducts } = useProducts();
+  const { availableProducts } = useProducts();
 
-    return (
-        <>
-            <main className="prod-content">
-                <FilterSideNav />
-                <section className="prod-section">
-                    <h3>Showing Products { `${availableProducts.length} / ${availableProducts.length}` }</h3>
-                    <div className="card-container">
-                        {
-                            availableProducts.map(({
-                                _id,
-                                title,
-                                price, 
-                                subtitle, 
-                                rating, 
-                                imgSrc
-                                }) => (
-                                <Card key={_id} price={price} title={title} subtitle={subtitle} rating={rating} imgSrc={imgSrc}/>
-                            ))}
-                    </div>
-                </section>
-            </main>
-        </>
-    );
-}
+  return (
+    <>
+      <main className="prod-content">
+        <FilterSideNav />
+        <section className="prod-section">
+          <h3>
+            Showing Products{" "}
+            {`${availableProducts.length} / ${availableProducts.length}`}
+          </h3>
+          <div className="card-container">
+            {availableProducts.map((item) => (
+              <Card {...item} />
+            ))}
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
 
 export { Products };
